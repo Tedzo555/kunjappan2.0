21d9954357393d349d2f55a4ff8479cde8a8fa6e
TEDZO123 <94947774+TEDZO123@users.noreply.github.com>
2022-05-11 17:34:17 +0530

    Update info.py

--- info.py
+++ info.py
@@ -33,12 +33,12 @@ AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "")
-DATABASE_NAME = environ.get('DATABASE_NAME', "kunjappan")
+DATABASE_NAME = environ.get('DATABASE_NAME', "tedzomovies01")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', 0))
-SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'kunjappansupport')
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'tzobotz')
 P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "False")), False)
 IMDB = is_enabled((environ.get('IMDB', "True")), True)
 SINGLE_BUTTON = is_enabled((environ.get('SINGLE_BUTTON', "False")), False)
