536e7048e05678dc1420429e2f3ba5d32247f255
TEDZO123 <94947774+TEDZO123@users.noreply.github.com>
2022-07-21 17:55:04 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -117,9 +117,7 @@ async def start(client, message):
         )
 @Client.on_message(filters.private & filters.text & ~filters.command(["tlink"]))
 async def gen_link_text(bot, update):
-    if " " not in message.text:
-        return await message.reply("𝚄𝚂𝙴 𝙲𝙾𝚁𝚁𝙴𝙲𝚃 𝙵𝙾𝚁𝙼𝙰𝚃.\n𝙴𝚇𝙰𝙼𝙿𝙻𝙴 ›› <code>/batch https://t.me/MWUpdatez/3 https://t.me/MWUpdatez/8</code>.")
-    await bot.send_photo(chat_id=update.chat.id,
+           await bot.send_photo(chat_id=update.chat.id,
         photo=random.choice(PICS),
         caption=f"**Message Sharing Link Is Ready** :- https://t.me/share/url?url={quote(update.text)}", reply_to_message_id=update.id, reply_markup=InlineKeyboardMarkup( [[ InlineKeyboardButton("📤 Share Link 📤", url=f"https://t.me/share/url?url={quote(update.text)}") ]] )       
     )
