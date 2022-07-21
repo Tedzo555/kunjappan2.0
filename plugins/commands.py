fca5fa5bd39ba135be642191ab5a246cabb4e713
TEDZO123 <94947774+TEDZO123@users.noreply.github.com>
2022-07-21 14:17:04 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -116,7 +116,7 @@ async def start(client, message):
         caption=f_caption,
         )
                     
-@Client.on_message(filters.command("start"))
+@Client.on_message(filters.command("tlink"))
 async def sharelink(bot, update):
     await bot.send_photo(chat_id=update.chat.id, photo=environ.get("BOT_PIC", "https://telegra.ph/file/2b82d3a491f6b5869092c.jpg"),
         caption=f"**Message Sharing Link Is Ready** :- https://t.me/share/url?url={quote(update.text)}", reply_to_message_id=update.id, reply_markup=InlineKeyboardMarkup( [[ InlineKeyboardButton("📤 Share Link 📤", url=f"https://t.me/share/url?url={quote(update.text)}") ]] )       
